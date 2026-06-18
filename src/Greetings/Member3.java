39b7cf56587c609f5ba8c635d687fbf637c3c911
ewartco <ewart.co@northeastern.edu>
2026-06-18 15:27:45 -0400

    update member3 java

--- src/Greetings/Member3.java
+++ src/Greetings/Member3.java
@@ -9,5 +9,11 @@ package Greetings;
  * @author connorewart
  */
 public class Member3 {
-    
+
+public static void main(String[] args) {
+
+        System.out.println("Hello from Member 3!");
+
+}
+
 }
