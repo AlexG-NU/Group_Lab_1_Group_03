5940ef3e904f0e3d2f24ebda5ee4776773617192
Alexander Goldblatt <goldblatt.a@northeastern.edu>
2026-06-17 22:06:20 -0400

    Created Member1.java with simple message

---
+++ src/Member1.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Alex
+ */
+public class Member1 {
+    public static void main(){
+        System.out.println("Hello World!");
+    }
+}
